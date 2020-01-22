26f994c80309cf63b4bff30e484a3bda0d76ffee
DaveP <dave@wearecrystal.uk>
2020-01-22 22:25:32 +0000

    Some responsive

--- src/views/User.vue
+++ src/views/User.vue
@@ -27,7 +27,11 @@ export default {
     <UserCard :user="user" />
 
     <div class="flex flex-wrap -mx-4">
-      <div v-for="item in inventory" :key="item.icon_url" class="w-1/5 p-4 mb-8 text-center">
+      <div
+        v-for="item in inventory"
+        :key="item.icon_url"
+        class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-4 mb-8 text-center"
+      >
         <InventoryImage :uri="item.icon_url_large" class="mb-4" />
         <p>{{ item.weapon }} | {{ item.skin }}</p>
         <span class="block text-xs">({{ item.wear }})</span>
